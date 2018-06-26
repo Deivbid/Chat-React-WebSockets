6bbf8886c952813faef6a51b3cfa5f6d370375df
David Aparicio <davidaapariciobak@gmail.com>
2018-06-25 21:23:31 -0400

    Store created and testing

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,16 @@
 import {OrderedMap} from 'immutable'
-export default class Store{
-    constructor(){
-        this.messages = OrderedMap();
-        this.channels = OrderedMap();
 
+const users = OrderedMap({
+    1: {_id: 1, name: "David", created: new Date()},
+    2: {_id: 2, name: "Crismary", created: new Date()},
+    3: {_id: 3, name: "Bruno", created: new Date()}
+})
+export default class Store{
+    constructor(appComponent){
+        this.app      = appComponent
+        this.messages = OrderedMap()
+        this.channels = OrderedMap()
+        this.activeChannelId = null
         this.user     = {
             _id: 0,
             name: 'Tom',
@@ -11,8 +18,15 @@ export default class Store{
         }
     }
 
+
+    getActiveChannel(){
+        const channel = this.activeChannelId ? this.channels.get(this.activeChannelId) : this.channels.first()
+        return channel 
+    }
     addMessage(index, message = {}){
         this.messages = this.messages.set(index, message)
+
+        this.update()
     }
 
     getMessages(){
@@ -24,6 +38,10 @@ export default class Store{
     }
 
     getChannels(){
-        return this.channels.valueSeq
+        return this.channels.valueSeq()
+    }
+
+    update(){
+        this.app.forceUpdate()
     }
 }
